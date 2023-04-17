53ffe921b09d0f8e2452271ddc3109d292336bc4
Ali Mohammad Saghiri <a_m_saghiri@yahoo.com>
2023-04-17 01:07:19 -0400

    Update PROJECT.sol

--- PROJECT.sol
+++ PROJECT.sol
@@ -3,23 +3,28 @@
 pragma solidity ^0.6.0;
 
 contract Transfer {
-   	address public sender;
+   	address sender;
     address payable public reciver;
     uint256 public cost;
     uint256 public balance;
     uint256 public reciverBalance;
+    uint256 public senderBalanceBefore;
+    uint256 public senderBalanceAfter;
 
-
-   
     function payment(uint256 _cost, address _sender, address payable _reciver) public 
     {
         reciver = payable(_reciver);
         sender = _sender;
-        cost = _cost;
-        balance= sender.balance;
-        require(sender.balance >= cost/10, "Insufficient balance");
-        reciver.transfer(cost/10);
-        reciverBalance = reciver.balance;
-
+        cost = _cost/ 1 ether;
+        balance = sender.balance/ 1 ether;
+        require(sender.balance >= cost/1 ether, "Insufficient balance");
+        senderBalanceBefore = sender.balance/ 1 ether;
+        reciver.transfer(cost);
+        senderBalanceAfter = sender.balance/ 1 ether;
+        
     }
+    receive() external payable {}
+
 }
+
+
